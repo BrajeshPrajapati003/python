5a1ab56564b0949fe4922f4b8c860fda5632c56e
Brajesh <prajapatibrajesh003@gmail.com>
2026-01-02 12:07:02 +0530

    day 1 completed

--- Hello.py
+++ Hello.py
@@ -1,4 +1,53 @@
-print("Python setup successful....!")
-x = 20
-y = 20
-print(x + y)
+# print("Python setup successful....!")
+
+                    # NOTE: Data Types
+
+# x = 20
+# y = 20
+# print(x + y)
+
+# print(type(x))
+
+# a = "brajesh"
+# print(type(a))
+
+# b = True
+# c = False
+# print(b-c)
+
+# d = 6 + 7j
+# print(type(d))
+
+# print(d.real, d.imag)
+
+                    #NOTE: String
+
+# s1 = "this IS a string"
+
+# print(type(s1))
+# print(len(s1))
+
+# s2 = " -- this is second string"
+
+# print(s1 + s2)
+# print(s1.find('s')) # 3
+# print(s1.find('x')) # -1
+# print(s1.count('i'))
+# s3 = s1.upper()
+# s4 = s1.lower()
+# print(s3, s4, s1, "---", s1.swapcase())
+# print(s1.title()) # capitalize the first letter of every word
+# print(s1.capitalize()) # first character have upper case and the rest lower case
+
+# print(s1[0])
+# print(s1[0:8:1])
+# print(s1[::-1])
+# print(s1[::-2])
+# print(s1[2::2])
+
+                    # NOTE: Lists
+
+list = []
+print(type(list))
+l1 = [67, 43, 66, "brajesh", 43.00]
+print(l1)
