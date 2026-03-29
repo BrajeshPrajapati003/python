ab302a003449016cd42669c0e379b52ca8a21bc0
Brajesh <prajapatibrajesh003@gmail.com>
2026-03-29 20:50:51 +0530

    greedy: 29 mar 2k26

--- cp/Greedy/Huffman_Coding.py
+++ cp/Greedy/Huffman_Coding.py
@@ -4,18 +4,29 @@ Goal:
     build a binary tree
     assign binary codes to characters
     minimize total encoding length
+Huffman Goal: minimize total bits required to encode data.
+
+frequent -> shorter, Rare -> longer 
+Total bits become less.
+
+Depth = code length
+if small values go deeper -> cost is small
+    so frequent nodes stay near root
+if big values go deeper -> cost becomes huge
+    so rare nodes go deeper
 '''
 import heapq
 
 def huffman_coding(freq):
     heap = freq[:]
-    heapq.heapify(heap)
+    heapq.heapify(heap) # add all frequencies
 
     total_cost = 0
 
+    # combine until one element left
     while len(heap) > 1:
-        a = heapq.heappop(heap)
-        b = heapq.heappop(heap)
+        a = heapq.heappop(heap) # smallest
+        b = heapq.heappop(heap) # 2nd smallest
 
         merged = a + b
         total_cost += merged
@@ -26,3 +37,6 @@ def huffman_coding(freq):
 
 freq = list(map(int, input().split()))
 print(huffman_coding(freq)) # 78
+
+
+# ! “Huffman coding minimizes encoding length by assigning shorter codes to frequent characters using a greedy approach of merging the smallest frequencies first.”
